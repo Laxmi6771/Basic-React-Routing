84b6331aa5605f760517e313b901a275248c29db
unknown <Laxmi6771>
2025-12-19 22:52:04 +0530

    Todo list with fetch, iteration and cleanup on unmount

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
 import { useState } from "react";
+import TodosList from "./TodosList";
 
 function App() {
-  const [isRed, setIsRed] = useState(true);
+  const [showTodos, setShowTodos] = useState(true);
 
   return (
     <div style={{ padding: "20px" }}>
-      <div
-        style={{
-          width: "200px",
-          height: "100px",
-          backgroundColor: isRed ? "red" : "blue",
-          color: "white",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          marginBottom: "10px",
-        }}
-      >
-        Color Toggle Div
-      </div>
-
-      <button onClick={() => setIsRed(!isRed)}>
-        Change Color
+      <button onClick={() => setShowTodos(false)}>
+        Unmount Todos
       </button>
+
+      <br /><br />
+
+      {showTodos && <TodosList />}
     </div>
   );
 }
