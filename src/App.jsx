ddab3a703ce9f36728f8bd28686e04e5ac9dce12
unknown <Laxmi6771>
2025-12-22 20:02:08 +0530

    Basic React Routing assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import Todos from "./components/Todos";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Todos from "./pages/Todos";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <Todos />;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/aboutus" element={<About />} />
+        <Route path="/todos" element={<Todos />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
-
-
-
